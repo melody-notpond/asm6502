35a744efe1b67ae4860e9a5ec70162d81f1c54b0
jenra <jenra.uwu@gmail.com>
2020-10-18 19:12:40 -0400

    Implemented test read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+use std::process;
+
+fn main()
+{
+	let args: Vec<String> = env::args().collect();
+
+	if args.len() <= 1
+	{
+		println!("usage: {} [files]", args[0]);
+		process::exit(1);
+	}
+
+	for file in &args[1..]
+	{
+		let content = fs::read_to_string(file).unwrap_or_else(|_| {
+			panic!("failed");
+		});
+
+		println!("Content:\n{}", content);
+	}
+
+	println!("{:?}", args);
 }
