11e58df1d972216feb384df5158f43ec8877af95
jenra <jenra.uwu@gmail.com>
2020-10-18 19:13:55 -0400

    Added example

---
+++ examples/hello.s
@@ -0,0 +1,20 @@
+; example from http://logicalmoon.com/2017/11/using-vs-code-to-create-a-6502-hello-world/
+
+oswrch = $FFEE
+
+.org $2000		 ; code origin (like P=$2000)
+
+_start:
+	LDX #$00
+letter:
+	LDA message, X
+	CMP #0
+	BEQ finished
+	JSR oswrch
+	INX
+	JMP letter
+finished:
+	RTS
+
+message:
+	.bytes "Hello, world", 13, 10, 0
