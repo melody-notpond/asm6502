9d5e66eaa8f07d8e7081e8e70c8199fce8594fbf
jenra <jenra.uwu@gmail.com>
2020-10-22 17:55:31 -0400

    Fixed overwriting previously written code

--- src/pass_2.rs
+++ src/pass_2.rs
@@ -17,18 +17,28 @@ use crate::pass_1::{
 pub struct SecondPassResult {
 	pub start: u16,
 	pub end: u16,
-	pub bytes: [u8; 65536]
+	pub bytes: [u8; u16::MAX as usize + 1]
+}
+
+// Check if code overwrites previously written code
+macro_rules! check_overwrite {
+	($bytes: ident, $line: ident, $index: expr) => {
+		if $bytes[$index as usize] != 0 {
+			return ParseError::new($line.lino, "This line overrides previously written code");
+		}
+	};
 }
 
 // Performs the second pass on the code
 pub fn second_pass(first_pass: FirstPassResult) -> Result<SecondPassResult, ParseError> {
 	let mut start = u16::MAX;
 	let mut end = 0u16;
-	let mut bytes = [0u8; 65536];
+	let mut bytes = [0u8; u16::MAX as usize + 1];
 
 	// Iterate over the lines of code
 	for line in first_pass.lines {
 		// Set the opcode
+		check_overwrite!(bytes, line, line.addr);
 		bytes[line.addr as usize] = line.opcode;
 
 		// Set the start index if necessary
@@ -47,13 +57,16 @@ pub fn second_pass(first_pass: FirstPassResult) -> Result<SecondPassResult, Pars
 			// 1 byte argument
 			InstructionArg::ByteArg(n) => {
 				last = line.addr + 1;
+				check_overwrite!(bytes, line, last);
 				bytes[last as usize] = n;
 			}
 
 			// 1 word argument
 			InstructionArg::WordArg(n) => {
 				last = line.addr + 2;
+				check_overwrite!(bytes, line, line.addr + 1);
 				bytes[line.addr as usize + 1] = n as u8;
+				check_overwrite!(bytes, line, last);
 				bytes[last as usize] = (n >> 8) as u8;
 			}
 
@@ -63,6 +76,7 @@ pub fn second_pass(first_pass: FirstPassResult) -> Result<SecondPassResult, Pars
 
 				if let Some(v) = first_pass.symbol_table.get(&label) {
 					if *v < 256 {
+						check_overwrite!(bytes, line, last);
 						bytes[last as usize] = *v as u8;
 					} else {
 						return ParseError::new(line.lino, &format!("Expected byte, found word {}", label));
@@ -77,6 +91,7 @@ pub fn second_pass(first_pass: FirstPassResult) -> Result<SecondPassResult, Pars
 				last = line.addr + 1;
 
 				if let Some(v) = first_pass.symbol_table.get(&label) {
+					check_overwrite!(bytes, line, last);
 					bytes[last as usize] = *v as u8;
 				} else {
 					return ParseError::new(line.lino, &format!("Undeclared label '{}' used as value", label));
@@ -88,6 +103,7 @@ pub fn second_pass(first_pass: FirstPassResult) -> Result<SecondPassResult, Pars
 				last = line.addr + 1;
 
 				if let Some(v) = first_pass.symbol_table.get(&label) {
+					check_overwrite!(bytes, line, last);
 					bytes[last as usize] = (*v >> 8) as u8;
 				} else {
 					return ParseError::new(line.lino, &format!("Undeclared label '{}' used as value", label));
@@ -99,7 +115,9 @@ pub fn second_pass(first_pass: FirstPassResult) -> Result<SecondPassResult, Pars
 				last = line.addr + 2;
 
 				if let Some(v) = first_pass.symbol_table.get(&label) {
+					check_overwrite!(bytes, line, line.addr + 1);
 					bytes[line.addr as usize + 1] = *v as u8;
+					check_overwrite!(bytes, line, last);
 					bytes[last as usize] = (*v >> 8) as u8;
 				} else {
 					return ParseError::new(line.lino, &format!("Undeclared label '{}' used as value", label));
@@ -113,6 +131,7 @@ pub fn second_pass(first_pass: FirstPassResult) -> Result<SecondPassResult, Pars
 				if let Some(v) = first_pass.symbol_table.get(&label) {
 					let diff = *v as i32 - line.addr as i32 + 2;
 					if -128 <= diff && diff <= 127 {
+						check_overwrite!(bytes, line, last);
 						bytes[last as usize] = diff as u8;
 					} else {
 						return ParseError::new(line.lino, &format!("Label '{}' is too far away", label));
